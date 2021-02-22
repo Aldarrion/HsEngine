0b8a0879137cd6ed2d2222f8332ae23700b426dc
Pavel Šmejkal <smejkal.pa@gmail.com>
2021-02-22 20:59:47 +0100

    Fix broken gui shader

--- Shaders/Gui_fs.hlsl
+++ Shaders/Gui_fs.hlsl
@@ -6,8 +6,6 @@ SamplerState GuiSampler : register(s0, space0);
 //------------------------------------------------------------------------------
 float4 main(ps_in input) : SV_Target
 {
-    eturn float4(1, 0, 0, 1);
-    //float4 col = GetTex2D(NonUniformResourceIndex(input.TexIdx)).Sample(GuiSampler, input.UV.xy);
     float4 col = BindlessTex2D[NonUniformResourceIndex(input.TexIdx)].Sample(GuiSampler, input.UV.xy);
     col *= input.Color;
 
