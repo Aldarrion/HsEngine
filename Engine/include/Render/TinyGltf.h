57deb03ee58881eb9584afc73e8b63d7f9c5f603
Pavel Šmejkal <smejkal.pa@gmail.com>
2022-01-12 21:57:43 +0100

    Add Tiny GLTF

---
+++ Engine/include/Render/TinyGltf.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#pragma warning(push)
+#pragma warning(disable : 4530)
+
+#include "tinygltf/tiny_gltf.h"
+
+#pragma warning(pop)
---
+++ Engine/include/World/World.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace hs
+{
+
+class World
+{
+    //
+};
+
+class Scene
+{
+    // Camera
+    // RenderTarget
+    // Objects to render
+};
+
+void GetObjectsToRenderFromWorld(World* world, Scene* scene)
+{
+    //
+}
+
+void SortObjectsInScene(Scene* scene);
+
+}
